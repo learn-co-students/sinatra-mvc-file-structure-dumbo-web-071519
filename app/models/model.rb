cc36a8af0bcd189938bf21a03a95128d5bc0e8a6
Megan Adams <meganoliviawendyadams@gmail.com>
2019-08-06 15:08:31 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,23 @@
-class Model
+class Dog 
+  
+  attr_accessor :name, :breed, :age
+  
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed 
+    @age = age 
+    DOGS << self 
+    end
+    
+  def self.all 
+    DOGS
+  end
+  
+#should hav name, breed and age attributes
+
+#keep track of each instance of dog created
+
+#and class method 'all' to return those  instances
 
 end
